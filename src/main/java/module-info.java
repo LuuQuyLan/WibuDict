16b6730e4de17ec77531f197abc83724b25a5223
TheDuyN17 <22024578@vnu.edu.vn>
2023-11-26 01:29:24 +0700

    tduy update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,15 @@
 module dictionary.dictionary {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
     requires org.json;
 
-    opens   dictionary.dictionary to javafx.fxml;
+    opens dictionary.dictionary to javafx.fxml;
     exports dictionary.dictionary;
-    exports dictionary.dictionary.DictionaryCommandLine;
+
     opens dictionary.dictionary.DictionaryCommandLine to javafx.fxml;
-    exports dictionary.dictionary.Controller;
+    exports dictionary.dictionary.DictionaryCommandLine;
+
     opens dictionary.dictionary.Controller to javafx.fxml;
+    exports dictionary.dictionary.Controller;
 }
